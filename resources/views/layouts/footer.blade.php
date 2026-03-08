fd6fe39e9bd930935bdc40c329ad89baff9b75ca
Ilham Dharma Atmaja <meniandol@gmail.com>
2026-03-08 23:03:15 +0700

    Job 5 Praktikum 1 - Layouting AdminLTE

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> 3.2.0
+  </div>
+  <strong>Copyright &copy; 2024 <a href="#">PWL POS</a>.</strong> All rights reserved.
+</footer>
